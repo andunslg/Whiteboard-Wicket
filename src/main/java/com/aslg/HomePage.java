c0c6b7921d707966b35188ca64cf5884353ae6b0
andunslg <andunslg@gmail.com>
2013-07-05 02:07:37 +0530

    Adding License

--- src/main/java/com/aslg/HomePage.java
+++ src/main/java/com/aslg/HomePage.java
@@ -1,3 +1,19 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.aslg;
 
 import org.apache.wicket.Component;
@@ -5,16 +21,8 @@ import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
-import org.apache.wicket.markup.html.IHeaderContributor;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.request.resource.JavaScriptResourceReference;
-import org.apache.wicket.util.template.PackageTextTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -34,9 +42,6 @@ public class HomePage extends WebPage {
 				super.renderHead(component,response);
 				String componentMarkupId = component.getMarkupId();
 				String callbackUrl = getCallbackUrl().toString();
-
-				System.out.println(componentMarkupId);
-				System.out.println(callbackUrl);
 
 				response.render(JavaScriptHeaderItem.forScript("var callbackUrl='"+callbackUrl+"';","urlHolder"));
 			}
